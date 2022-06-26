b4a9de34c8e6f93db97dba2477e94948f0774b4a
imads33 <imadsab33@gmail.com>
2022-06-26 10:30:08 +0530

    project creation commit

--- aacv/settings.py
+++ aacv/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@z(91*!%%19vph(=x3=h@mwb3+5tng7&*ew-l@d#t#pwn(-1%'
+SECRET_KEY = 'django-insecure-w4ougg4qcawp3%u900k4y81hrq)%-a8t=u-t3g-j_q1vm$juh#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
